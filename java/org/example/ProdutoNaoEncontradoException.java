df26b39b42a25681fa9f0dd40d369bd709268454
mattvnn <matteovanin321@gmail.com>
2025-04-11 00:30:37 -0300

    Adiciona modelo de produto (Produto, ProdutoPerecivel, ProdutoNaoPerecivel)

--- java/org/example/ProdutoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class ProdutoNaoEncontradoException extends Exception {
-    public ProdutoNaoEncontradoException(String message) {
-        super(message);
-    }
-}
--- java/org/example/ProdutoNaoPerecivel.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class ProdutoNaoPerecivel extends Produto {
-
-    public ProdutoNaoPerecivel(String nome, int quantidade, Categoria categoria, Fornecedor fornecedor) {
-        super(nome, quantidade, categoria, fornecedor);
-    }
-}
